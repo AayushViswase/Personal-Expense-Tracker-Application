643926d2a1692ab3a233f819f723fda8fb45f64d
AayushViswase <aayushviswase10@gmail.com>
2023-12-03 20:57:59 +0530

    Working code

--- Node.js project/app.js
+++ Node.js project/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/expense_tracker', {
+mongoose.connect('mongodb://localhost/27017', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -15,17 +18,14 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
+  // Start your server or perform additional setup here
 });
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
+// Define routes (this is where you'll add your route configuration)
 const transactionRoutes = require('./routes/transactionRoutes');
 app.use('/transactions', transactionRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
